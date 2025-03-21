a6dc1dd71af78f0dfd51aec9140fd4662e2a5bab
laaroussi <dali.laaroussi@gmail.com>
2025-03-21 01:47:50 +0100

    first commit

--- src/main/java/com/example/livraison/application/config/SwaggerConfig.java
+++ src/main/java/com/example/livraison/application/config/SwaggerConfig.java
@@ -2,6 +2,8 @@ package com.example.livraison.application.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -14,6 +16,13 @@ public class SwaggerConfig {
                 .info(new Info()
                         .title("Livraison API")
                         .version("1.0")
-                        .description("API pour la gestion des livraisons"));
+                        .description("API pour la gestion des livraisons"))
+                .addSecurityItem(new SecurityRequirement().addList("BearerAuth"))
+                .components(new io.swagger.v3.oas.models.Components()
+                        .addSecuritySchemes("BearerAuth", new SecurityScheme()
+                                .name("BearerAuth")
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")));
     }
 }
